dc4288be326396272f4e97ce108b423410e75a0c
Nocthus <58665599+Nocthus@users.noreply.github.com>
2021-07-06 14:41:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using FubarDev.FtpServer;
+using FubarDev.FtpServer.FileSystem.DotNet;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace TestDotNetHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+               .ConfigureServices(
+                    services =>
+                    {
+                        // Add FTP server services
+                        // DotNetFileSystemProvider = Use the .NET file system functionality
+                        // AnonymousMembershipProvider = allow only anonymous logins
+                        services
+                           .AddFtpServer(
+                                builder => builder
+                                   .UseDotNetFileSystem()
+                                   .EnableAnonymousAuthentication());
+
+                        // Configure the FTP server
+                        services.Configure<FtpServerOptions>(opt => opt.ServerAddress = "*");
+
+                        // use %TEMP%/TestFtpServer as root folder
+                        services.Configure<DotNetFileSystemOptions>(opt => opt
+                            .RootPath = Path.Combine(Path.GetTempPath(), "TestFtpServer"));
+
+                        
+                        services
+                           .AddHostedService<HostedFtpService>();
+                    })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Service/WebRequestSaveGet.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Net;
+
+namespace TestDotNetHost
+{
+    public class WebRequestSave
+    {
+        public static void WebRequestSaveGet ()
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://www.contoso.com/test.htm");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("anonymous","janeDoe@contoso.com");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.WriteLine($"Download Complete, status {response.StatusDescription}");
+
+            reader.Close();
+            response.Close();
+        }
+    }
+}
